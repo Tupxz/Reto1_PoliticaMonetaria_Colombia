25986cfcacebba29b54a30b7dbeaaa6370da705a
Santiago Tupaz <santi.tupaz@gmail.com>
2026-02-08 21:11:22 -0500

    corección

--- Scripts/01_packages.R
+++ Scripts/01_packages.R
@@ -3,19 +3,21 @@
 # Instalación y carga de paquetes necesarios
 ############################################################
 
-# Vector de paquetes requeridos
+# Lista de paquetes requeridos
 packages <- c(
-  "tidyverse",  # Manipulación y visualización de datos
-  "lubridate",  # Manejo de fechas y tiempos
-  "vars",       # Modelos VAR
-  "tseries",    # Series de tiempo
-  "readxl"      # Lectura de archivos Excel
+  "tidyverse",
+  "lubridate",
+  "vars",
+  "tseries",
+  "readxl"
 )
 
-# Instala los paquetes que no estén instalados
-installed <- packages %in% rownames(installed.packages())
-if (any(!installed)) {
-  install.packages(packages[!installed])
+# Identifica cuáles paquetes no están instalados
+not_installed <- packages[!(packages %in% rownames(installed.packages()))]
+
+# Instala solo los que faltan
+if(length(not_installed)) {
+  install.packages(not_installed)
 }
 
 # Carga todos los paquetes
